011fe2a7d662d95bc8efa85512cf626803f0326a
Strange <168190107+fxgurv@users.noreply.github.com>
2024-09-06 02:11:15 -0700

    Update YouTube.py

--- MoneyPrinterV3/src/classes/YouTube.py
+++ MoneyPrinterV3/src/classes/YouTube.py
@@ -15,19 +15,11 @@ from constants import *
 from typing import List
 from moviepy.editor import *
 from termcolor import colored
-# from selenium_firefox import *
-# from selenium import webdriver
 from moviepy.video.fx.all import crop
 from moviepy.config import change_settings
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.chrome.service import Service
-# from selenium.webdriver.chrome.options import Options
 from moviepy.video.tools.subtitles import SubtitlesClip
-# from webdriver_manager.chrome import ChromeDriverManager
 from datetime import datetime
 from .GeneratorImg import Generation
-
-
 import moviepy.editor as mp
 import math
 from PIL import Image
@@ -40,6 +32,7 @@ mygenerator = Generation()
 
 # Set ImageMagick Path
 change_settings({"IMAGEMAGICK_BINARY": get_imagemagick_path()})
+
 
 def get_segments(input_str):
     regex = r"{ \"(.|\n)*"
@@ -79,21 +72,13 @@ def remove_g4f_finishreason(input_str):
     if match_end != match_start != 0:
         test_str = test_str[:match_start]
         print('test_str after edit: ', test_str)
-    # Solve encrypt
-    # test_str = re.sub(r'[^\w\s.?!]', '', test_str)
     pattern = r'\$\@\$(v=v\d+\.\d+-rv\d+|v=undefined-rv\d+)\$\@\$(.*?)'
     cleaned_text = re.sub(pattern, '', test_str)
     return cleaned_text
 
-import math
-# from .BingImagesFetcher import BingImagesFetcher
-
-# fetcher_img = BingImagesFetcher()
-
 class YouTube:
     """
     Class for YouTube Automation.
-
     Steps to create a YouTube Short:
     1. Generate a topic [DONE]
     2. Generate a script [DONE]
@@ -125,17 +110,6 @@ class YouTube:
         self._language: str = language
 
         self.images = []
-
-        # Initialize the Firefox profile
-        # self.options: Options = Options()
-        
-        # self.options.add_argument('--headless')
-        # self.options.add_argument('--no-sandbox')
-        # self.options.add_argument('--disable-dev-shm-usage')
-
-        # self.service: Service = Service(ChromeDriverManager().install())
-
-        # self.browser: webdriver.Chrome = webdriver.Chrome(service=self.service, options=self.options)
 
     @property
     def niche(self) -> str:
@@ -241,10 +215,7 @@ class YouTube:
         Subject: {self.subject}
         Language: {self.language}
         """
-        # print('prompt generate_script: ', prompt)
         completion = self.generate_response(prompt)
-
-        # Apply regex to remove *
         completion = re.sub(r"\*", "", completion)
         print('completion generate_script: ', completion)
         
@@ -269,11 +240,6 @@ class YouTube:
             metadata (dict): The generated metadata.
         """
         title = self.generate_response(f"Please generate a YouTube Video Title for the following subject, including hashtags: {self.subject}. Only return the title, nothing else. Limit the title under 100 characters.")
-        
-        # if len(title) > 100:
-        #     if get_verbose():
-        #         warning("Generated Title is too long. Retrying...")
-        #     return self.generate_metadata()
         print('title: ', title)
         description = self.generate_response(f"Please generate a YouTube Video Description for the following script: {self.script}. Only return the description, nothing else.")
         print('description: ', description)
@@ -293,7 +259,6 @@ class YouTube:
         Returns:
             image_prompts (List[str]): Generated List of image prompts.
         """
-        # n_prompts = math.floor(len(self.script) / 3)
         n_prompts = 10
 
         prompt = f"""
@@ -328,11 +293,8 @@ class YouTube:
             .replace("```json", "") \
             .replace("```", "")
 
-        # completion = remove_g4f_finishreason(completion)
         image_prompts = []
         print('completion generate_prompts: ', completion)
-        # completion = re.sub(r'[^\w\s.?!]', '', completion)
-        # completion = completion.split('.')
         print('completion generate_prompts images: ', completion)
         if "image_prompts" in completion:
             image_prompts = json.loads(completion)["image_prompts"]
@@ -347,8 +309,6 @@ class YouTube:
                 print('what exception: ', str(e))
                 if get_verbose():
                     warning("GPT returned an unformatted response. Attempting to clean...")
-
-                # Get everything between [ and ], and turn it into a list
                 r = re.compile(r"\[.*\]")
                 image_prompts = r.findall(completion)
                 if len(image_prompts) == 0:
@@ -360,8 +320,6 @@ class YouTube:
         print('self image_prompts: ', self.image_prompts)
         print('n_prompts: ', n_prompts)
         print('lenimageprompts: ', len(self.image_prompts))
-        # Check the amount of image prompts
-        # and remove if it's more than needed
         if len(self.image_prompts) > n_prompts:
             self.image_prompts = self.image_prompts[:n_prompts]
 
@@ -381,60 +339,12 @@ class YouTube:
         """
         ok = False
         while ok == False:
-            # url = f"https://hercai.onrender.com/{get_image_model()}/text2image?prompt={prompt}"
-
-            # r = requests.get(url)
-            # parsed = r.json()
-
-            # if "url" not in parsed or not parsed.get("url"):
-            #     # Retry
-            #     if get_verbose():
-            #         info(f" => Failed to generate Image for Prompt: {prompt}. Retrying...")
-            #     ok = False
-            # else:
-            #     ok = True
-            #     image_url = parsed["url"]
-
-            #     if get_verbose():
-            #         info(f" => Generated Image: {image_url}")
-
-            #     image_path = os.path.join(ROOT_DIR, ".mp", str(uuid4()) + ".png")
-                
-            #     with open(image_path, "wb") as image_file:
-            #         # Write bytes to file
-            #         image_r = requests.get(image_url)
-
-            #         image_file.write(image_r.content)
-
-            #     if get_verbose():
-            #         info(f" => Wrote Image to \"{image_path}\"\n")
-
-            #     self.images.append(image_path)
-                
-            #     return image_path
             ok = True
-
-            # Method 2
-            # image_path_bytes = fetcher_img.generate(prompt)
-            # print('image _ path fetcher: ', image_path_bytes)
-            # image_path = os.path.join(ROOT_DIR, ".mp", str(uuid4()) + ".png")
-                
-            # with open(image_path, "wb") as image_file:
-            #     with open(image_path_bytes, "rb") as read_image_file:
-            #         # Write bytes to file
-            #         image_file.write(read_image_file.read())
-            # if get_verbose():
-            #     info(f" => Wrote Image to \"{image_path}\"\n")
-
-            # Method 1
             bytes_content = mygenerator.create(prompt)
 
             image_path = os.path.join(ROOT_DIR, ".mp", str(uuid4()) + ".png")
-                
             with open(image_path, "wb") as image_file:
-                # Write bytes to file
                 image_file.write(bytes_content)
-
             if get_verbose():
                 info(f" => Wrote Image to \"{image_path}\"\n")
             save_image(image_path, folder_name)
@@ -453,17 +363,12 @@ class YouTube:
             path_to_wav (str): Path to generated audio (WAV Format).
         """
         path = os.path.join(ROOT_DIR, ".mp", str(uuid4()) + ".wav")
-
         # Clean script, remove every character that is not a word character, a space, a period, a question mark, or an exclamation mark.
         self.script = re.sub(r'[^\w\s.?!]', '', self.script)
-
         tts_instance.synthesize(self.script, path)
-
         self.tts_path = path
-
         if get_verbose():
             info(f" => Wrote TTS to \"{path}\"")
-
         return path
     
     def add_video(self, video: dict) -> None:
@@ -743,13 +648,6 @@ def zoom_in_effect(clip, zoom_ratio=0.04):
         Returns:
             channel_id (str): The Channel ID.
         """
-        # driver = self.browser
-        # driver.get("https://studio.youtube.com")
-        # time.sleep(2)
-        # channel_id = driver.current_url.split("/")[-1]
-        # self.channel_id = channel_id
-
-        # return channel_id
         return None
 
     def upload_video(self) -> bool:
@@ -759,142 +657,6 @@ def zoom_in_effect(clip, zoom_ratio=0.04):
         Returns:
             success (bool): Whether the upload was successful or not.
         """
-        # try:
-        #     self.get_channel_id()
-
-        #     driver = self.browser
-        #     verbose = get_verbose()
-
-        #     # Go to youtube.com/upload
-        #     driver.get("https://www.youtube.com/upload")
-
-        #     # Set video file
-        #     FILE_PICKER_TAG = "ytcp-uploads-file-picker"
-        #     file_picker = driver.find_element(By.TAG_NAME, FILE_PICKER_TAG)
-        #     INPUT_TAG = "input"
-        #     file_input = file_picker.find_element(By.TAG_NAME, INPUT_TAG)
-        #     file_input.send_keys(self.video_path)
-
-        #     # Wait for upload to finish
-        #     time.sleep(5)
-
-        #     # Set title
-        #     textboxes = driver.find_elements(By.ID, YOUTUBE_TEXTBOX_ID)
-
-        #     title_el = textboxes[0]
-        #     description_el = textboxes[-1]
-
-        #     if verbose:
-        #         info("\t=> Setting title...")
-
-        #     title_el.click()
-        #     time.sleep(1)
-        #     title_el.clear()
-        #     title_el.send_keys(self.metadata["title"])
-
-        #     if verbose:
-        #         info("\t=> Setting description...")
-
-        #     # Set description
-        #     time.sleep(10)
-        #     description_el.click()
-        #     time.sleep(0.5)
-        #     description_el.clear()
-        #     description_el.send_keys(self.metadata["description"])
-
-        #     time.sleep(0.5)
-
-        #     # Set `made for kids` option
-        #     if verbose:
-        #         info("\t=> Setting `made for kids` option...")
-
-        #     is_for_kids_checkbox = driver.find_element(By.NAME, YOUTUBE_MADE_FOR_KIDS_NAME)
-        #     is_not_for_kids_checkbox = driver.find_element(By.NAME, YOUTUBE_NOT_MADE_FOR_KIDS_NAME)
-
-        #     if not get_is_for_kids():
-        #         is_not_for_kids_checkbox.click()
-        #     else:
-        #         is_for_kids_checkbox.click()
-
-        #     time.sleep(0.5)
-
-        #     # Click next
-        #     if verbose:
-        #         info("\t=> Clicking next...")
-
-        #     next_button = driver.find_element(By.ID, YOUTUBE_NEXT_BUTTON_ID)
-        #     next_button.click()
-
-        #     # Click next again
-        #     if verbose:
-        #         info("\t=> Clicking next again...")
-        #     next_button = driver.find_element(By.ID, YOUTUBE_NEXT_BUTTON_ID)
-        #     next_button.click()
-
-        #     # Wait for 2 seconds
-        #     time.sleep(2)
-
-        #     # Click next again
-        #     if verbose:
-        #         info("\t=> Clicking next again...")
-        #     next_button = driver.find_element(By.ID, YOUTUBE_NEXT_BUTTON_ID)
-        #     next_button.click()
-
-        #     # Set as unlisted
-        #     if verbose:
-        #         info("\t=> Setting as unlisted...")
-            
-        #     radio_button = driver.find_elements(By.XPATH, YOUTUBE_RADIO_BUTTON_XPATH)
-        #     radio_button[2].click()
-
-        #     if verbose:
-        #         info("\t=> Clicking done button...")
-
-        #     # Click done button
-        #     done_button = driver.find_element(By.ID, YOUTUBE_DONE_BUTTON_ID)
-        #     done_button.click()
-
-        #     # Wait for 2 seconds
-        #     time.sleep(2)
-
-        #     # Get latest video
-        #     if verbose:
-        #         info("\t=> Getting video URL...")
-
-        #     # Get the latest uploaded video URL
-        #     driver.get(f"https://studio.youtube.com/channel/{self.channel_id}/videos/short")
-        #     time.sleep(2)
-        #     videos = driver.find_elements(By.TAG_NAME, "ytcp-video-row")
-        #     first_video = videos[0]
-        #     anchor_tag = first_video.find_element(By.TAG_NAME, "a")
-        #     href = anchor_tag.get_attribute("href")
-        #     if verbose:
-        #         info(f"\t=> Extracting video ID from URL: {href}")
-        #     video_id = href.split("/")[-2]
-
-        #     # Build URL
-        #     url = build_url(video_id)
-
-        #     self.uploaded_video_url = url
-
-        #     if verbose:
-        #         success(f" => Uploaded Video: {url}")
-
-        #     # Add video to cache
-        #     self.add_video({
-        #         "title": self.metadata["title"],
-        #         "description": self.metadata["description"],
-        #         "url": url,
-        #         "date": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        #     })
-
-        #     # Close the browser
-        #     driver.quit()
-
-        #     return True
-        # except:
-        #     self.browser.quit()
-        #     return False
         return False
 
 
@@ -905,23 +667,4 @@ def zoom_in_effect(clip, zoom_ratio=0.04):
         Returns:
             videos (List[dict]): The uploaded videos.
         """
-        # if not os.path.exists(get_youtube_cache_path()):
-        #     # Create the cache file
-        #     with open(get_youtube_cache_path(), 'w') as file:
-        #         json.dump({
-        #             "videos": []
-        #         }, file, indent=4)
-        #     return []
-
-        # videos = []
-        # # Read the cache file
-        # with open(get_youtube_cache_path(), 'r') as file:
-        #     previous_json = json.loads(file.read())
-        #     # Find our account
-        #     accounts = previous_json["accounts"]
-        #     for account in accounts:
-        #         if account["id"] == self._account_uuid:
-        #             videos = account["videos"]
-
-        # return videos
         return []
