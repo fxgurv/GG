045490c324571c54086210cc94f6de7d2ecec62b
Strange <168190107+fxgurv@users.noreply.github.com>
2024-09-06 02:00:41 -0700

    Update YouTube.py

--- MoneyPrinterV3/src/classes/YouTube.py
+++ MoneyPrinterV3/src/classes/YouTube.py
@@ -34,35 +34,7 @@ from PIL import Image
 import numpy
 from save_short import save_image
 
-def zoom_in_effect(clip, zoom_ratio=0.04):
-    def effect(get_frame, t):
-        img = Image.fromarray(get_frame(t))
-        base_size = img.size
 
-        new_size = [
-            math.ceil(img.size[0] * (1 + (zoom_ratio * t))),
-            math.ceil(img.size[1] * (1 + (zoom_ratio * t)))
-        ]
-
-        # The new dimensions must be even.
-        new_size[0] = new_size[0] + (new_size[0] % 2)
-        new_size[1] = new_size[1] + (new_size[1] % 2)
-
-        img = img.resize(new_size, Image.LANCZOS)
-
-        x = math.ceil((new_size[0] - base_size[0]) / 2)
-        y = math.ceil((new_size[1] - base_size[1]) / 2)
-
-        img = img.crop([
-            x, y, new_size[0] - x, new_size[1] - y
-        ]).resize(base_size, Image.LANCZOS)
-
-        result = numpy.array(img)
-        img.close()
-
-        return result
-
-    return clip.fl(effect)
 
 mygenerator = Generation()
 
@@ -545,6 +517,39 @@ class YouTube:
             file.write(subtitles)
 
         return srt_path
+
+
+
+def zoom_in_effect(clip, zoom_ratio=0.04):
+    def effect(get_frame, t):
+        img = Image.fromarray(get_frame(t))
+        base_size = img.size
+
+        new_size = [
+            math.ceil(img.size[0] * (1 + (zoom_ratio * t))),
+            math.ceil(img.size[1] * (1 + (zoom_ratio * t)))
+        ]
+
+        # The new dimensions must be even.
+        new_size[0] = new_size[0] + (new_size[0] % 2)
+        new_size[1] = new_size[1] + (new_size[1] % 2)
+
+        img = img.resize(new_size, Image.LANCZOS)
+
+        x = math.ceil((new_size[0] - base_size[0]) / 2)
+        y = math.ceil((new_size[1] - base_size[1]) / 2)
+
+        img = img.crop([
+            x, y, new_size[0] - x, new_size[1] - y
+        ]).resize(base_size, Image.LANCZOS)
+
+        result = numpy.array(img)
+        img.close()
+
+        return result
+
+    return clip.fl(effect)
+    
 
     def combine(self) -> str:
         """
