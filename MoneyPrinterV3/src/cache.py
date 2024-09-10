600a8e647e1143869f3a98d29cb44c537b213e1f
Strange <168190107+fxgurv@users.noreply.github.com>
2024-09-10 04:06:53 -0700

    Update cache.py

--- MoneyPrinterV3/src/cache.py
+++ MoneyPrinterV3/src/cache.py
@@ -1,8 +1,9 @@
 import os
 import json
-
 from typing import List
 from config import ROOT_DIR
+
+
 
 def get_cache_path() -> str:
     """
@@ -13,23 +14,6 @@ def get_cache_path() -> str:
     """
     return os.path.join(ROOT_DIR, '.mp')
 
-def get_afm_cache_path() -> str:
-    """
-    Gets the path to the Affiliate Marketing cache file.
-
-    Returns:
-        path (str): The path to the AFM cache folder
-    """
-    return os.path.join(get_cache_path(), 'afm.json')
-
-def get_twitter_cache_path() -> str:
-    """
-    Gets the path to the Twitter cache file.
-
-    Returns:
-        path (str): The path to the Twitter cache folder
-    """
-    return os.path.join(get_cache_path(), 'twitter.json')
 
 def get_youtube_cache_path() -> str:
     """
@@ -133,47 +117,7 @@ def remove_account(account_id: str) -> None:
             "accounts": accounts
         }, file, indent=4)
 
-def get_products() -> List[dict]:
-    """
-    Gets the products from the cache.
 
-    Returns:
-        products (List[dict]): The products
-    """
-    if not os.path.exists(get_afm_cache_path()):
-        # Create the cache file
-        with open(get_afm_cache_path(), 'w') as file:
-            json.dump({
-                "products": []
-            }, file, indent=4)
-
-    with open(get_afm_cache_path(), 'r') as file:
-        parsed = json.load(file)
-
-        # Get the products
-        return parsed["products"]
-    
-def add_product(product: dict) -> None:
-    """
-    Adds a product to the cache.
-
-    Args:
-        product (dict): The product to add
-
-    Returns:
-        None
-    """
-    # Get the current products
-    products = get_products()
-
-    # Add the new product
-    products.append(product)
-
-    # Write the new products to the cache
-    with open(get_afm_cache_path(), 'w') as file:
-        json.dump({
-            "products": products
-        }, file, indent=4)
     
 def get_results_cache_path() -> str:
     """
